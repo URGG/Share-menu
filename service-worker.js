0e91a8478f6ffb1eeef909a8e789883b76c7110b
URGG <georgeurgiles2@gmail.com>
2025-06-25 09:47:08 -0700

    adding the pwa

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,45 @@
 // service-worker.js
+const CACHE_NAME = 'leslie-business-card-v1';
+const urlsToCache = [
+  'https://ads4u.store/leslie-business-card/',
+  // Add other important resources Leslie's main page needs
+  // 'https://ads4u.store/path-to-leslie-image.jpg',
+  // 'https://ads4u.store/path-to-styles.css'
+];
+
 self.addEventListener("install", (e) => {
   console.log("Service Worker Installed");
-  e.waitUntil(self.skipWaiting());
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener("activate", (e) => {
   console.log("Service Worker Activated");
-  return self.clients.claim();
-}); 
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        return response || fetch(e.request);
+      })
+  );
+});
